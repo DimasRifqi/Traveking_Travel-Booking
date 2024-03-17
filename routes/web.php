4950bb2ddc7b71e334dc3bc0b2b71845e999c9bf
DimasRifqi <dimasrifqiramadhani@gmail.com>
2024-03-17 22:40:49 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+// Route::get('/halamanbeli', function () {
+//     return view('halamanbeli');
+// });
